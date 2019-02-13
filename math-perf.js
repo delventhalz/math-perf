e6baacdad8d6bd0c2a3856774235d38da20bf767
Zac Delventhal <delventhalz@gmail.com>
2019-02-13 12:48:36 -0600

    Wrap both JS files in combined Node/IIFE modules
    The source of `math-perf.js` is now fully portable between the browser
    and Node. It exports three properties either under the `MathPerf` global
    object or as a CommonJS module:
     - recentOutputs: recently output number arrays for debugging
     - tests: an object the source for each test (moved from view.js)
     - runTest: takes a string for the test name and input and runs test loops
    
    As a consequence of this commit, `view.js` will no longer expose any
    global objects.
    
    Also times are now in nanoseconds not microseconds.
    
    Signed-off-by: Zac Delventhal <delventhalz@gmail.com>

--- math-perf.js
+++ math-perf.js
@@ -1,95 +1,142 @@
-'use strict';
+;(function() {
+  'use strict';
 
-const LUT_RES = 1024;
+  const LUT_RES = 1024;
+  const RAND_CACHE_SIZE = 65521;
 
-// Largest prime under 2^16
-const RAND_CACHE_SIZE = 65521;
+  const recentOutputs = {};
+  const range = (len, fn) => Array(...Array(len)).map((_, i) => fn(i));
 
-window.outputs = {};
-
-const noop = n => n;
-const range = (len, fn) => Array(...Array(len)).map((_, i) => fn(i));
-
-const getTrigArray = trigFn => range(LUT_RES, i => trigFn((i * 2 * Math.PI / LUT_RES)));
-const LOOKUP = {
-  SIN: getTrigArray(Math.sin),
-  COS: getTrigArray(Math.cos),
-  TAN: getTrigArray(Math.tan),
-};
-
-const getRandCache = xformFn => range(RAND_CACHE_SIZE, () => xformFn(Math.random()));
-const RAND_CACHES = {
-  uint8: getRandCache(r => Math.floor(r * 256)),
-  uint32: getRandCache(r => Math.floor(r * 4294967296)),
-  maxint: getRandCache(r => Math.floor(r * Number.MAX_SAFE_INTEGER)),
-  float: getRandCache(r => r * Number.MAX_SAFE_INTEGER),
-};
-
-// Gets a function which iterates over the appropriate rand cache indefinitely
-const getRandFn = (type) => {
-  const cache = RAND_CACHES[type];
-  let i = -1;
-  return () => {
-    i = i < RAND_CACHE_SIZE ? i + 1 : 0;
-    return cache[i];
+  // Tests
+  const getTrigArray = trigFn => range(LUT_RES, i => trigFn((i * 2 * Math.PI / LUT_RES)));
+  const LOOKUP = {
+    SIN: getTrigArray(Math.sin),
+    COS: getTrigArray(Math.cos),
+    TAN: getTrigArray(Math.tan),
   };
-};
 
-const loopTest = (name, input, testFn) => {
-  window.outputs[name] = [];
-  const outputs = window.outputs[name];
-  const rand = getRandFn(input);
-  const stop = Date.now() + 2500;
+  const tests = {
+    noop: n => n,
+    add: n => n + 113,
+    multiply: n => n * 113,
+    divide: n => n / 113,
+    modulo: n => n % 113,
 
-  // Test Loop
-  while (Date.now() < stop) {
-    outputs.push(testFn(rand()));
+    power: n => n ** 113,
+    pow: n => Math.pow(n,113),
+    sqr: n => n * n,
+    sqrt: n => Math.sqrt(n),
+
+    sin: n => Math.sin(n),
+    cos: n => Math.cos(n),
+    tan: n => Math.tan(n),
+    'sin-lut': n => LOOKUP.SIN[Math.floor((n / (2 * Math.PI) % 1) * LUT_RES)],
+    'cos-lut': n => LOOKUP.COS[Math.floor((n / (2 * Math.PI) % 1) * LUT_RES)],
+    'tan-lut': n => LOOKUP.TAN[Math.floor((n / (2 * Math.PI) % 1) * LUT_RES)],
+
+    gt: n => n > 113,
+    lte: n => n <= 113,
+    eq: n => n === 113,
+
+    floor: n => Math.floor(n),
+    round: n => Math.round(n),
+    'floor-bit': n => n | 0,
+    abs: n => Math.abs(n),
+    'abs-bit': n => { const mask = n >> 31; return (mask ^ n) - mask; },
+    'is-odd': n => n % 2,
+    'is-odd-bit': n => n & 1,
+
+    quadratic: n => Math.pow(n, 2) + 17 * n - 113,
+    factors: n => (n + 17) * (n - 113),
+    'ten-ops': n =>  (n - 23) / (n + 17) * (n + 19) / (n - 13) * (n - 29) - 113,
+    'is-shorter-sqrt': n => Math.sqrt(Math.pow(n - 17, 2) + Math.pow(n - 13, 2)) < 113,
+    'is-shorter-sqr': n => Math.pow(n - 17, 2) + Math.pow(n - 13, 2) < Math.pow(113, 2),
+  };
+
+  // Random Inputs
+  const getRandCache = xformFn => range(RAND_CACHE_SIZE, () => xformFn(Math.random()));
+  const RAND_CACHES = {
+    uint8: getRandCache(r => Math.floor(r * 256)),
+    uint32: getRandCache(r => Math.floor(r * 4294967296)),
+    maxint: getRandCache(r => Math.floor(r * Number.MAX_SAFE_INTEGER)),
+    float: getRandCache(r => r * Number.MAX_SAFE_INTEGER),
+  };
+
+  const getRandFn = (type) => {
+    const cache = RAND_CACHES[type];
+    let i = -1;
+    return () => {
+      i = i < RAND_CACHE_SIZE ? i + 1 : 0;
+      return cache[i];
+    };
+  };
+
+  // Running Tests
+  const loopTest = (name, input) => {
+    recentOutputs[name] = [];
+    const outputs = recentOutputs[name];
+    const test = tests[name];
+    const rand = getRandFn(input);
+    const stop = Date.now() + 2500;
+
+    // Test Loop
+    while (Date.now() < stop) {
+      outputs.push(test(rand()));
+    }
+
+    const runs = outputs.length;
+    console.log(`${name}(${input}) runs:`, runs.toLocaleString());
+    return runs;
+  };
+
+  const runTest = (name, input) => {
+    console.log(`-------- testing ${name}(${input}) --------`);
+
+    // Alternate between a test run and a noop run in alternating order
+    const noopRun1 = loopTest('noop', input);
+    const testRun1 = loopTest(name, input);
+    const testRun2 = loopTest(name, input);
+    const noopRun2 = loopTest('noop', input);
+    const noopRun3 = loopTest('noop', input);
+    const testRun3 = loopTest(name, input);
+    const testRun4 = loopTest(name, input);
+    const noopRun4 = loopTest('noop', input);
+
+    const noopRuns = noopRun1 + noopRun2 + noopRun3 + noopRun4;
+    const testRuns = testRun1 + testRun2 + testRun3 + testRun4;
+
+    // 10,000,000,000 (i.e. 10 billion nanoseconds)
+    const noopDuration = 10000000000 / noopRuns;
+    const testDuration = 10000000000 / testRuns;
+
+    const results = {
+      runs: testRuns,
+      duration: testDuration,
+      noloop: testDuration - noopDuration,
+      ratio: testDuration / noopDuration,
+    }
+
+    console.log(`\n${name}(${input}) total runs      :`, results.runs.toLocaleString());
+    console.log(`${name}(${input}) duration (raw)    :`, `${results.duration.toFixed(2)}ns`);
+    console.log(`${name}(${input}) duration (no loop):`, `${results.noloop.toFixed(2)}ns`);
+    console.log(`${name}(${input}) duration (ratio)  :`, `${results.ratio.toFixed(2)}x`);
+    console.log('-------------------------------------');
+
+    return results;
+  };
+
+  // Exports
+  const toExport = {
+    recentOutputs,
+    tests,
+    runTest,
+  };
+
+  if (typeof window !== 'undefined') {
+    window.MathPerf = toExport;
   }
 
-  const runs = outputs.length;
-  console.log(`${name}(${input}) runs:`, runs.toLocaleString());
-  return runs;
-};
-
-// Run the test function and a noop control in an alternating pattern
-// and print the results to the screen and console
-const getTestRunner = (name, testFn) => () => {
-  const input = document.getElementById(`${name}-select`).value;
-  console.log(`-------- testing ${name}(${input}) --------`);
-
-  // Alternate between a test run and a noop run in alternating order
-  const noopRun1 = loopTest('noop', input, noop);
-  const testRun1 = loopTest(name, input, testFn);
-  const testRun2 = loopTest(name, input, testFn);
-  const noopRun2 = loopTest('noop', input, noop);
-  const noopRun3 = loopTest('noop', input, noop);
-  const testRun3 = loopTest(name, input, testFn);
-  const testRun4 = loopTest(name, input, testFn);
-  const noopRun4 = loopTest('noop', input, noop);
-
-  // Calculate results
-  const noopRuns = noopRun1 + noopRun2 + noopRun3 + noopRun4;
-  const testRuns = testRun1 + testRun2 + testRun3 + testRun4;
-  const noopDuration = 10000000 / noopRuns;
-  const testDuration = 10000000 / testRuns;
-
-  // Format results
-  const runsOutput = testRuns.toLocaleString();
-  const durationOutput = `${testDuration.toFixed(3)}μs`;
-  const noLoopOutput = `${(testDuration - noopDuration).toFixed(3)}μs`;
-  const ratioOutput = `${(testDuration / noopDuration).toFixed(2)}x`;
-
-  // Log results
-  console.log(`\n${name}(${input}) total runs      :`, runsOutput);
-  console.log(`${name}(${input}) duration (raw)    :`, durationOutput);
-  console.log(`${name}(${input}) duration (no loop):`, noLoopOutput);
-  console.log(`${name}(${input}) duration (ratio)  :`, ratioOutput);
-
-  // Write results to UI
-  document.getElementById(`${name}-result-runs`).innerText = runsOutput;
-  document.getElementById(`${name}-result-raw`).innerText = durationOutput;
-  document.getElementById(`${name}-result-no-loop`).innerText = noLoopOutput;
-  document.getElementById(`${name}-result-ratio`).innerText = ratioOutput;
-
-  console.log('-------------------------------------');
-};
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = toExport;
+  }
+})();
--- view.js
+++ view.js
@@ -1,144 +1,157 @@
-'use strict';
+;(function() {
+  'use strict';
 
-// Creates an element, short name makes it easy to nest many calls
-const e = (tag, { onclick, ...attrs }, ...children) => {
-  const elem = document.createElement(tag);
-  for (const [key, value] of Object.entries(attrs)) {
-    elem.setAttribute(key, value);
-  }
-  if (onclick) {
-    elem.onclick = onclick;
-  }
-  elem.append(...children);
-  return elem;
-};
+  // Creates an element, short name makes it easy to nest many calls
+  const e = (tag, { onclick, ...attrs }, ...children) => {
+    const elem = document.createElement(tag);
+    for (const [key, value] of Object.entries(attrs)) {
+      elem.setAttribute(key, value);
+    }
+    if (onclick) {
+      elem.onclick = onclick;
+    }
+    elem.append(...children);
+    return elem;
+  };
 
-// UI Components
-const getDefListItem = (key, definition) => (
-  e('li', {},
-    e('em', {}, `${key}:`),
-    ` ${definition}`));
+  const getTestRunner = (name) => () => {
+    const input = document.getElementById(`${name}-select`).value;
 
-const getSection = (header, ...children) => (
-  e('div', { class: 'section' },
-    e('h2', {}, header),
-    ...children,
-    e('hr', {})));
+    const { runs, duration, noloop, ratio } = MathPerf.runTest(name, input);
 
-const getFnSource = (fn) => (
-  e('span', { style: 'font-family:monospace;font-style:oblique;color:#888;' },
-    fn.toString()));
+    document.getElementById(`${name}-result-runs`).innerText = runs.toLocaleString();
+    document.getElementById(`${name}-result-raw`).innerText = `${duration.toFixed(2)}ns`;
+    document.getElementById(`${name}-result-no-loop`).innerText = `${noloop.toFixed(2)}ns`;
+    document.getElementById(`${name}-result-ratio`).innerText = `${ratio.toFixed(2)}x`;
+  };
 
-const getInputSelect = (name) => (
-  e('select', { id: `${name}-select` },
-    e('option', { value: 'uint8' }, 'Uint8'),
-    e('option', { value: 'uint32' }, 'Uint32'),
-    e('option', { value: 'maxint' }, 'Max Safe Int'),
-    e('option', { value: 'float' }, 'Float')));
+  // UI Components
+  const getDefListItem = (key, definition) => (
+    e('li', {},
+      e('em', {}, `${key}:`),
+      ` ${definition}`));
 
-const getRunButton = (name, testFn) => (
-  e('button', {
-    id: `${name}-button`,
-    style: 'margin-left:3em;',
-    onclick: getTestRunner(name, testFn),
-  }, 'Run'));
+  const getSection = (header, ...children) => (
+    e('div', { class: 'section' },
+      e('h2', {}, header),
+      ...children,
+      e('hr', {})));
 
-const getOutputLine = (label, id) => (
-  e('div', {},
-    e('span', { style: 'margin-right:1em;' }, label),
-    e('em', { id })));
+  const getFnSource = (name) => (
+    e('span', { style: 'font-family:monospace;font-style:oblique;color:#888;' },
+      MathPerf.tests[name].toString()));
 
-const getTestComponent = (name, testFn) => (
-  e('div', { class: 'perf-runner', style: 'margin-top:2em;' },
-    e('h3', {}, name),
-    e('div', { style: 'margin-bottom:1em;' },
-      getFnSource(testFn)),
-    e('div', { style: 'margin-bottom:1em;' },
-      e('strong', { style: 'margin-right:1em;' }, 'Inputs:'),
-      getInputSelect(name),
-      getRunButton(name, testFn)),
-    getOutputLine('Number of runs in 10s :', `${name}-result-runs`),
-    getOutputLine('Duration per run (raw):', `${name}-result-raw`),
-    getOutputLine('Without loop (approx.):', `${name}-result-no-loop`),
-    getOutputLine('Ratio to noop control :', `${name}-result-ratio`)));
+  const getInputSelect = (name) => (
+    e('select', { id: `${name}-select` },
+      e('option', { value: 'uint8' }, 'Uint8'),
+      e('option', { value: 'uint32' }, 'Uint32'),
+      e('option', { value: 'maxint' }, 'Max Safe Int'),
+      e('option', { value: 'float' }, 'Float')));
 
-// Append UI to DOM
-document.getElementById('app').append(
-  e('div', { class: 'header' },
-    e('h1', {}, 'Math Perf'),
+  const getRunButton = (name) => (
+    e('button', {
+      id: `${name}-button`,
+      style: 'margin-left:3em;',
+      onclick: getTestRunner(name),
+    }, 'Run'));
 
-    e('p', {},
-      'This page is a simple tool to try to test the performance of various ',
-      'numerical operations across a variety of browsers and devices. ',
-      'Clicking "Run", will loop the specified function repeatedly for ',
-      '10 seconds against random inputs. It will also loop a simple noop',
-      'function for 10 seconds as a control.'),
+  const getOutputLine = (label, id) => (
+    e('div', {},
+      e('span', { style: 'margin-right:1em;' }, label),
+      e('em', { id })));
 
-    e('p', {}, 'The Inputs:',
-      e('ul', {},
-        getDefListItem('Uint8', 'Random whole numbers from 0 - 255'),
-        getDefListItem('Uint32', 'Random whole numbers from 0 - 4,294,967,295'),
-        getDefListItem('Max Safe Int', 'Random whole numbers from 0 - 9,007,199,254,740,990'),
-        getDefListItem('Float', 'Random decimal numbers from 0 - 9,007,199,254,740,990'))),
+  const getTestComponent = (name) => (
+    e('div', { class: 'perf-runner', style: 'margin-top:2em;' },
+      e('h3', {}, name),
+      e('div', { style: 'margin-bottom:1em;' },
+        getFnSource(name)),
+      e('div', { style: 'margin-bottom:1em;' },
+        e('strong', { style: 'margin-right:1em;' }, 'Inputs:'),
+        getInputSelect(name),
+        getRunButton(name)),
+      getOutputLine('Number of runs in 10s :', `${name}-result-runs`),
+      getOutputLine('Duration per run (raw):', `${name}-result-raw`),
+      getOutputLine('Without loop (approx.):', `${name}-result-no-loop`),
+      getOutputLine('Ratio to noop control :', `${name}-result-ratio`)));
 
-    e('p', {}, 'The Outputs:',
-      e('ul', {},
-        getDefListItem(
-          'Number of runs',
-          'Number of loops completed in 10 seconds'),
-        getDefListItem(
-          'Duration per run',
-          'Raw duration per whole loop iteration in microseconds'),
-        getDefListItem(
-          'Without loop',
-          'Tries to get actual operation duration by subtracting noop time'),
-        getDefListItem(
-          'Ratio to noop',
-          'How many times slower than the noop loop each run was (lower better)')))),
+  // Append UI to DOM
+  document.getElementById('app').append(
+    e('div', { class: 'header' },
+      e('h1', {}, 'Math Perf'),
 
-  getSection(
-    'Arithmetic',
-    getTestComponent('add', n => n + 113),
-    getTestComponent('multiply', n => n * 113),
-    getTestComponent('divide', n => n / 113),
-    getTestComponent('modulo', n => n % 113)),
+      e('p', {},
+        'This page is a simple tool to try to test the performance of various ',
+        'numerical operations across a variety of browsers and devices. ',
+        'Clicking "Run", will loop the specified function repeatedly for ',
+        '10 seconds against random inputs. It will also loop a simple noop',
+        'function for 10 seconds as a control.'),
 
-  getSection(
-    'Higher Math',
-    getTestComponent('power', n => n ** 113),
-    getTestComponent('pow', n => Math.pow(n,113)),
-    getTestComponent('sqr', n => n * n),
-    getTestComponent('sqrt', n => Math.sqrt(n))),
+      e('p', {}, 'The Inputs:',
+        e('ul', {},
+          getDefListItem('Uint8', 'Random whole numbers from 0 - 255'),
+          getDefListItem('Uint32', 'Random whole numbers from 0 - 4,294,967,295'),
+          getDefListItem('Max Safe Int', 'Random whole numbers from 0 - 9,007,199,254,740,990'),
+          getDefListItem('Float', 'Random decimal numbers from 0 - 9,007,199,254,740,990'))),
 
-  getSection(
-    'Trig Functions',
-    getTestComponent('sin', n => Math.sin(n)),
-    getTestComponent('cos', n => Math.cos(n)),
-    getTestComponent('tan', n => Math.tan(n)),
-    getTestComponent('sin-lut', n => LOOKUP.SIN[Math.floor((n / (2 * Math.PI) % 1) * LUT_RES)]),
-    getTestComponent('cos-lut', n => LOOKUP.COS[Math.floor((n / (2 * Math.PI) % 1) * LUT_RES)]),
-    getTestComponent('tan-lut', n => LOOKUP.TAN[Math.floor((n / (2 * Math.PI) % 1) * LUT_RES)])),
+      e('p', {}, 'The Outputs:',
+        e('ul', {},
+          getDefListItem(
+            'Number of runs',
+            'Number of loops completed in 10 seconds'),
+          getDefListItem(
+            'Duration per run',
+            'Raw duration per whole loop iteration in nanoseconds'),
+          getDefListItem(
+            'Without loop',
+            'Tries to get actual operation duration by subtracting noop time'),
+          getDefListItem(
+            'Ratio to noop',
+            'How many times slower than the noop loop each run was (lower better)')))),
 
-  getSection(
-    'Logic',
-    getTestComponent('gt', n => n > 113),
-    getTestComponent('lte', n => n <= 113),
-    getTestComponent('eq', n => n === 113)),
+    getSection(
+      'Arithmetic',
+      getTestComponent('add'),
+      getTestComponent('multiply'),
+      getTestComponent('divide'),
+      getTestComponent('modulo')),
 
-  getSection(
-    'Number Manipulation',
-    getTestComponent('floor', n => Math.floor(n)),
-    getTestComponent('round', n => Math.round(n)),
-    getTestComponent('floor-bit', n => n | 0),
-    getTestComponent('abs', n => Math.abs(n)),
-    getTestComponent('abs-bit', n => { const mask = n >> 31; return (mask ^ n) - mask; }),
-    getTestComponent('is-odd', n => n % 2),
-    getTestComponent('is-odd-bit', n => n & 1)),
+    getSection(
+      'Higher Math',
+      getTestComponent('power'),
+      getTestComponent('pow'),
+      getTestComponent('sqr'),
+      getTestComponent('sqrt')),
 
-  getSection(
-    'Polynomials',
-    getTestComponent('quadratic', n => Math.pow(n, 2) + 17 * n - 113),
-    getTestComponent('factors', n => (n + 17) * (n - 113)),
-    getTestComponent('ten-ops', n =>  (n - 23) / (n + 17) * (n + 19) / (n - 13) * (n - 29) - 113),
-    getTestComponent('is-shorter-sqrt', n => Math.sqrt(Math.pow(n - 17, 2) + Math.pow(n - 13, 2)) < 113, 5),
-    getTestComponent('is-shorter-sqr', n => Math.pow(n - 17, 2) + Math.pow(n - 13, 2) < Math.pow(113, 2), 5)));
+    getSection(
+      'Trig Functions',
+      getTestComponent('sin'),
+      getTestComponent('cos'),
+      getTestComponent('tan'),
+      getTestComponent('sin-lut'),
+      getTestComponent('cos-lut'),
+      getTestComponent('tan-lut')),
+
+    getSection(
+      'Logic',
+      getTestComponent('gt'),
+      getTestComponent('lte'),
+      getTestComponent('eq')),
+
+    getSection(
+      'Number Manipulation',
+      getTestComponent('floor'),
+      getTestComponent('round'),
+      getTestComponent('floor-bit'),
+      getTestComponent('abs'),
+      getTestComponent('abs-bit'),
+      getTestComponent('is-odd'),
+      getTestComponent('is-odd-bit')),
+
+    getSection(
+      'Polynomials',
+      getTestComponent('quadratic'),
+      getTestComponent('factors'),
+      getTestComponent('ten-ops'),
+      getTestComponent('is-shorter-sqrt'),
+      getTestComponent('is-shorter-sqr')));
+})();
