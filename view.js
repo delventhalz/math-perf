dc0af1144ea39a50bdd8e56d7df11d5919d38777
Zac Delventhal <delventhalz@gmail.com>
2019-02-13 11:46:05 -0600

    Refactor view code to be more readable
    Signed-off-by: Zac Delventhal <delventhalz@gmail.com>

--- view.js
+++ view.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Create an element, short name makes it easy to nest many calls
+// Creates an element, short name makes it easy to nest many calls
 const e = (tag, { onclick, ...attrs }, ...children) => {
   const elem = document.createElement(tag);
   for (const [key, value] of Object.entries(attrs)) {
@@ -14,6 +14,17 @@ const e = (tag, { onclick, ...attrs }, ...children) => {
 };
 
 // UI Components
+const getDefListItem = (key, definition) => (
+  e('li', {},
+    e('em', {}, `${key}:`),
+    ` ${definition}`));
+
+const getSection = (header, ...children) => (
+  e('div', { class: 'section' },
+    e('h2', {}, header),
+    ...children,
+    e('hr', {})));
+
 const getFnSource = (fn) => (
   e('span', { style: 'font-family:monospace;font-style:oblique;color:#888;' },
     fn.toString()));
@@ -55,84 +66,79 @@ const getTestComponent = (name, testFn) => (
 document.getElementById('app').append(
   e('div', { class: 'header' },
     e('h1', {}, 'Math Perf'),
+
     e('p', {},
       'This page is a simple tool to try to test the performance of various ',
       'numerical operations across a variety of browsers and devices. ',
       'Clicking "Run", will loop the specified function repeatedly for ',
       '10 seconds against random inputs. It will also loop a simple noop',
       'function for 10 seconds as a control.'),
+
     e('p', {}, 'The Inputs:',
       e('ul', {},
-        e('li', {},
-          e('em', {}, 'Uint8:'),
-          ' Random whole numbers from 0 - 255'),
-        e('li', {},
-          e('em', {}, 'Uint32:'),
-          ' Random whole numbers from 0 - 4,294,967,295'),
-        e('li', {},
-          e('em', {}, 'Max Safe Int:'),
-          ' Random whole numbers from 0 - 9,007,199,254,740,990'),
-        e('li', {},
-          e('em', {}, 'Float:'),
-          ' Random decimal numbers from 0 - 9,007,199,254,740,990'))),
+        getDefListItem('Uint8', 'Random whole numbers from 0 - 255'),
+        getDefListItem('Uint32', 'Random whole numbers from 0 - 4,294,967,295'),
+        getDefListItem('Max Safe Int', 'Random whole numbers from 0 - 9,007,199,254,740,990'),
+        getDefListItem('Float', 'Random decimal numbers from 0 - 9,007,199,254,740,990'))),
+
     e('p', {}, 'The Outputs:',
       e('ul', {},
-        e('li', {},
-          e('em', {}, 'Number of runs:'),
-          ' Number of loops completed in 10 seconds'),
-        e('li', {},
-          e('em', {}, 'Duration per run:'),
-          ' Raw duration per whole loop iteration in microseconds'),
-        e('li', {},
-          e('em', {}, 'Without loop:'),
-          ' Subtracts average noop loop duration (questionable accuracy)'),
-        e('li', {},
-          e('em', {}, 'Ratio to noop:'),
-          ' How many times slower than the noop loop each run was')))),
-  e('div', { class: 'section' },
-    e('h2', {}, 'Arithmetic'),
+        getDefListItem(
+          'Number of runs',
+          'Number of loops completed in 10 seconds'),
+        getDefListItem(
+          'Duration per run',
+          'Raw duration per whole loop iteration in microseconds'),
+        getDefListItem(
+          'Without loop',
+          'Tries to get actual operation duration by subtracting noop time'),
+        getDefListItem(
+          'Ratio to noop',
+          'How many times slower than the noop loop each run was (lower better)')))),
+
+  getSection(
+    'Arithmetic',
     getTestComponent('add', n => n + 113),
     getTestComponent('multiply', n => n * 113),
     getTestComponent('divide', n => n / 113),
-    getTestComponent('modulo', n => n % 113),
-    e('hr', {})),
-  e('div', { class: 'section' },
-    e('h2', {}, 'Higher Maths'),
+    getTestComponent('modulo', n => n % 113)),
+
+  getSection(
+    'Higher Math',
     getTestComponent('power', n => n ** 113),
     getTestComponent('pow', n => Math.pow(n,113)),
     getTestComponent('sqr', n => n * n),
-    getTestComponent('sqrt', n => Math.sqrt(n)),
-    e('hr', {})),
-  e('div', { class: 'section' },
-    e('h2', {}, 'Trig Functions'),
+    getTestComponent('sqrt', n => Math.sqrt(n))),
+
+  getSection(
+    'Trig Functions',
     getTestComponent('sin', n => Math.sin(n)),
     getTestComponent('cos', n => Math.cos(n)),
     getTestComponent('tan', n => Math.tan(n)),
     getTestComponent('sin-lut', n => LOOKUP.SIN[Math.floor((n / (2 * Math.PI) % 1) * LUT_RES)]),
     getTestComponent('cos-lut', n => LOOKUP.COS[Math.floor((n / (2 * Math.PI) % 1) * LUT_RES)]),
-    getTestComponent('tan-lut', n => LOOKUP.TAN[Math.floor((n / (2 * Math.PI) % 1) * LUT_RES)]),
-    e('hr', {})),
-  e('div', { class: 'section' },
-    e('h2', {}, 'Logic'),
+    getTestComponent('tan-lut', n => LOOKUP.TAN[Math.floor((n / (2 * Math.PI) % 1) * LUT_RES)])),
+
+  getSection(
+    'Logic',
     getTestComponent('gt', n => n > 113),
     getTestComponent('lte', n => n <= 113),
-    getTestComponent('eq', n => n === 113),
-    e('hr', {})),
-  e('div', { class: 'section' },
-    e('h2', {}, 'Number Manipulation'),
+    getTestComponent('eq', n => n === 113)),
+
+  getSection(
+    'Number Manipulation',
     getTestComponent('floor', n => Math.floor(n)),
     getTestComponent('round', n => Math.round(n)),
     getTestComponent('floor-bit', n => n | 0),
     getTestComponent('abs', n => Math.abs(n)),
     getTestComponent('abs-bit', n => { const mask = n >> 31; return (mask ^ n) - mask; }),
     getTestComponent('is-odd', n => n % 2),
-    getTestComponent('is-odd-bit', n => n & 1),
-    e('hr', {})),
-  e('div', { class: 'section' },
-    e('h2', {}, 'Polynomials'),
+    getTestComponent('is-odd-bit', n => n & 1)),
+
+  getSection(
+    'Polynomials',
     getTestComponent('quadratic', n => Math.pow(n, 2) + 17 * n - 113),
     getTestComponent('factors', n => (n + 17) * (n - 113)),
     getTestComponent('ten-ops', n =>  (n - 23) / (n + 17) * (n + 19) / (n - 13) * (n - 29) - 113),
     getTestComponent('is-shorter-sqrt', n => Math.sqrt(Math.pow(n - 17, 2) + Math.pow(n - 13, 2)) < 113, 5),
-    getTestComponent('is-shorter-sqr', n => Math.pow(n - 17, 2) + Math.pow(n - 13, 2) < Math.pow(113, 2), 5),
-    e('hr', {})),);
+    getTestComponent('is-shorter-sqr', n => Math.pow(n - 17, 2) + Math.pow(n - 13, 2) < Math.pow(113, 2), 5)));
